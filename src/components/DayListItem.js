eb3071eafe3de51c2761920429854e667b9f0095
Matthew Cook <matthewcook13987@gmail.com>
2021-03-15 16:36:12 -0500

    Updated daylistitem to include a spot formatter to pass new set of tests.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,7 +4,17 @@ import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
 
-  let dayClass = classNames(
+  const formatSpots = function(spots) {
+    if (spots == 1) {
+      return `${spots} spot remaining`
+    } else if (spots >= 2) {
+      return `${spots} spots remaining`
+    } else {
+      return `no spots remaining`
+    }
+  }
+
+  const dayClass = classNames(
     'day-list__item', 
     {'day-list__item--selected': props.selected},
     {'day-list__item--full': props.spots})
@@ -12,7 +22,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={()=> {props.setDay(props.name)}}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} Spots Remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
     );
 }
