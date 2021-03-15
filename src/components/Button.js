5013d073a253e71298c52dfceaa6ab4f9a82fe62
Matthew Cook <matthewcook13987@gmail.com>
2021-03-15 16:20:55 -0500

    Created dayListItem component with some styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,26 @@
 import React from "react";
+import classNames from 'classnames';
 
 import "components/Button.scss";
 
 export default function Button(props) {
+   
+   let buttonClass = classNames(
+   'button', 
+   {'button--confirm': props.confirm},
+   {'button--danger': props.danger})
 
-   let buttonClass = 'button';
 
-   if (props.confirm) {
-      buttonClass += ' button--confirm';
-   }
-   if (props.danger) {
-      buttonClass += ' button--danger';
-   }
+
+   classNames('foo', { bar: false }); // => 'foo'
+   //let buttonClass = 'button';
+
+   // if (props.confirm) {
+   //    buttonClass += ' button--confirm';
+   // }
+   // if (props.danger) {
+   //    buttonClass += ' button--danger';
+   // }
 
    return <>
       <button 
