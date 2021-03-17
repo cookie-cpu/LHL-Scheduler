a62de6eab470024a5f54990b2f3d2d5e3f09ed47
Matthew Cook <matthewcook13987@gmail.com>
2021-03-17 16:58:12 -0500

    Created folder for helpers with first helper function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,26 @@
+
+
+// finding the object in our state.days array who's name matches the provided day. 
+
+// With this information we can now access that specific days appointment array.
+
+// Once we have access to the appointment array for the given day, we'll need to iterate through it, 
+
+// comparing where it's id matches the id of states.appointments and return that value.
+
+// If there are no appointments on the given day, our days data will be empty we should return an empty array.
+
+
+export function getAppointmentsForDay(state, day) {
+ 
+  const output = [];
+
+  for (let appointDay of state.days){
+    if (appointDay.name === day){
+      for (const val of appointDay.appointments) {
+        output.push(state.appointments[val]);
+      }
+    }
+  }
+  return output;
+}
