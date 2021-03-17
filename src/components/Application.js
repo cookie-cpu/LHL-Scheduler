9dcf216a5473283461409e6f7cfce053c815b260
Matthew Cook <matthewcook13987@gmail.com>
2021-03-17 18:50:13 -0500

    created getInterview helper to parse the state object and return the interview data

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from 'components/DayList'
 import Appointment from './Appointment'
-import getAppointmentsForDay from '../helpers/selectors'
+import {getAppointmentsForDay, getInterview} from '../helpers/selectors'
 const axios = require('axios');
 
 /* API Routes
@@ -18,9 +18,9 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
-  const dailyAppointments = [];
 
   const setDay = day => setState({ ...state, day });
   // const setDays = (days) => setState((previous) => ({ ...previous, days }));
@@ -29,21 +29,39 @@ export default function Application(props) {
 
     Promise.all([
       axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments')
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers')
     ])
     .then((response)=>{
+      console.log(response)
      setState(previous => (
        {...previous,
         days:response[0].data,
-        appointments:response[1].data}))
+        appointments:response[1].data,
+        interviewers:response[2].data}))
     })
   }, [])
 
   
   
-  const listOfAppointments = getAppointmentsForDay(state, state.day).map((appointment)=>{
+  // const listOfAppointments = getAppointmentsForDay(state, state.day).map((appointment)=>{
+  //   return (
+  //     <Appointment key={appointment.id} {...appointment} />
+  //   )
+  // })
+
+  const appointments = getAppointmentsForDay(state,);
+
+  const schedule = appointments.map((appointment)=>{
+    const interview = getInterview(state, appointment.interview);
+
     return (
-      <Appointment key={appointment.id} {...appointment} />
+      <Appointment
+       key={appointment.id}
+       id={appointment.id}
+       time={appointment.time}
+       interview={interview}
+       />
     )
   })
 
@@ -73,7 +91,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-        {listOfAppointments}
+        {schedule}
         <Appointment key="last" time="7pm" />
       </section>
 
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,17 +1,5 @@
 
-
-// finding the object in our state.days array who's name matches the provided day. 
-
-// With this information we can now access that specific days appointment array.
-
-// Once we have access to the appointment array for the given day, we'll need to iterate through it, 
-
-// comparing where it's id matches the id of states.appointments and return that value.
-
-// If there are no appointments on the given day, our days data will be empty we should return an empty array.
-
-
-export default function getAppointmentsForDay(state, day) {
+export function getAppointmentsForDay(state, day) {
  
   const output = [];
 
@@ -22,5 +10,17 @@ export default function getAppointmentsForDay(state, day) {
       }
     }
   }
+  return output;
+}
+
+export function getInterview(state, interview){
+   if (!interview) {
+    return null;
+  }
+
+  const output = {};
+  output.student = interview.student;
+  output.interviewer = state.interviewers[interview.interviewer];
+
   return output;
 }
