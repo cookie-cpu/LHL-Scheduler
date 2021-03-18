c1e8c87fb35d080b500b7bb0b7fb9c5c60527437
Matthew Cook <matthewcook13987@gmail.com>
2021-03-18 19:55:47 +0000

    Created helper hook of useVisualMode to help transition page elements between different states

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,17 +1,20 @@
 import React, { Fragment } from 'react';
+import useVisualMode from '../../hooks/useVisualMode'
 import "../Appointment/styles.scss";
 import Header from '../Appointment/Header'
 import Show from '../Appointment/Show'
 import Empty from '../Appointment/Empty'
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
   return (
     <article className="appointment">
       <Header time={props.time} />
       {props.interview ? <Show
-      student={props.interview.student}
-      interviewer={props.interview.interviewer}
-       /> : <Empty />}
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      /> : <Empty />}
 
     </article>
   );
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+export default function useVisualMode(initialmode) {
+
+  const [mode, setMode] = useState(initialmode);
+  const [history, setHistory] = useState([initialmode])
+
+
+  function transition(newmode, replace = false) {
+    if (replace) {
+      history.pop();
+    }
+    setMode(newmode)
+
+    history.push(newmode);
+
+    setHistory(history);
+  }
+
+  function back() {
+    if (history.length > 1){
+      history.pop();
+
+      setHistory(history);
+
+      setMode(history[history.length - 1]);
+
+    } else {
+      setMode(mode);
+    }
+   
+  }
+
+
+  return { mode, transition, back };
+
+};
