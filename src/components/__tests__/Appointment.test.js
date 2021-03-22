eb5539dced9afa81263d0e30fa479f7b7333cb07
Matthew Cook <matthewcook13987@gmail.com>
2021-03-22 20:39:41 +0000

    Added first test for appointment component

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { render, cleanup } from "@testing-library/react";
+
+import Appointment from '../Appointment';
+
+afterEach(cleanup);
+
+describe("Appointment", () => {
+
+
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+
+
+
+
+
+
+
+  
+});
